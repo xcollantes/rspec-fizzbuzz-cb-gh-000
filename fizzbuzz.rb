d12f1d8fa0cb747169cc577549aa1828e937fae0
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-08-23 03:33:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,15 @@
 def fizzbuzz(num)
   div_by_three = num % 3
   div_by_five = num % 5
-  
-  if div_by_three == 0
+
+  if div_by_three == 0 && div_by_five == 0
+    puts "FizzBuzz"
+  elsif div_by_three == 0
     puts "Fizz"
 
   elsif div_by_five == 0
     puts "Buzz"
-    
+
+  
   end
 end
